a6bb51f0317d568916aa30f5f565e1700cd16e84
socoldkiller <2119306250@qq.com>
2025-05-14 02:08:32 +0800

    refactor: move queue

--- main.go
+++ main.go
@@ -4,33 +4,11 @@ import (
 	"context"
 	"fmt"
 	"strings"
-	"sync"
 )
 
 type Session struct {
 	GroupID int
 	UserID  int
-}
-
-type MsgQueue struct {
-	q     sync.Map
-	queue map[Session]chan *NapCatResponse
-}
-
-func (m *MsgQueue) Store(k Session, v chan *NapCatResponse) {
-	m.q.Store(k, v)
-}
-
-func (m *MsgQueue) Load(k Session) (chan *NapCatResponse, bool) {
-	r, ok := m.q.Load(k)
-	if !ok {
-		return nil, ok
-	}
-	return r.(chan *NapCatResponse), ok
-}
-
-func (m *MsgQueue) Delete(k Session) {
-	m.q.Delete(k)
 }
 
 func main() {
---
+++ queue.go
@@ -0,0 +1,24 @@
+package main
+
+import "sync"
+
+type MsgQueue struct {
+	q     sync.Map
+	queue map[Session]chan *NapCatResponse
+}
+
+func (m *MsgQueue) Store(k Session, v chan *NapCatResponse) {
+	m.q.Store(k, v)
+}
+
+func (m *MsgQueue) Load(k Session) (chan *NapCatResponse, bool) {
+	r, ok := m.q.Load(k)
+	if !ok {
+		return nil, ok
+	}
+	return r.(chan *NapCatResponse), ok
+}
+
+func (m *MsgQueue) Delete(k Session) {
+	m.q.Delete(k)
+}
