97f988ebe41c2f163954c281e7c061ad2b52870c
socoldkiller <2119306250@qq.com>
2025-05-24 21:24:46 +0800

    refactor: remove reconnect

--- ws.go
+++ ws.go
@@ -4,27 +4,21 @@ import (
 	"context"
 	"github.com/coder/websocket"
 	"github.com/sirupsen/logrus"
-	"sync"
 	"time"
 )
 
 type Websocket struct {
-	URL      string
-	ctx      context.Context
-	failChan chan error
-	conn     *websocket.Conn
-	rw       sync.RWMutex
+	URL  string
+	ctx  context.Context
+	conn *websocket.Conn
 }
 
 func NewWebSocket(ctx context.Context, url string) *Websocket {
-	conn := restyWS(url)
 	ws := &Websocket{
-		URL:      url,
-		ctx:      ctx,
-		conn:     conn,
-		failChan: make(chan error),
+		URL:  url,
+		ctx:  ctx,
+		conn: restyWS(url),
 	}
-	go ws.monitorReConnect()
 	return ws
 }
 
@@ -51,43 +45,21 @@ func restyWS(url string) *websocket.Conn {
 	}
 }
 
-func (ws *Websocket) monitorReConnect() {
-	for err := range ws.failChan {
-		logrus.Warnf("websocket  error :%v", err.Error())
-		ws.rw.Lock()
-		conn := restyWS(ws.URL)
-		logrus.Infof("websocket  success")
-		ws.conn = conn
-		ws.rw.Unlock()
-	}
-	return
-}
-
 func (ws *Websocket) Write(ctx context.Context, typ websocket.MessageType, p []byte) {
-	for {
-		ws.rw.Lock()
-		conn := ws.conn
-		ws.rw.Unlock()
-		err := conn.Write(ctx, typ, p)
-		if err != nil {
-			ws.failChan <- err
-		} else {
-			break
-		}
+	if err := ws.conn.Write(ctx, typ, p); err != nil {
+		logrus.Warnf("websocket conn can't write msg:(%s)", string(p))
 	}
 }
 
 func (ws *Websocket) Read(ctx context.Context) (websocket.MessageType, []byte) {
-	for {
-		ws.rw.Lock()
-		conn := ws.conn
-		ws.rw.Unlock()
-		typ, p, err := conn.Read(ctx)
-		if err != nil {
-			ws.failChan <- err
+	var (
+		err  error
+		typ  websocket.MessageType
+		body []byte
+	)
 
-		} else {
-			return typ, p
-		}
+	if typ, body, err = ws.conn.Read(ctx); err != nil {
+		logrus.Warnf("websocket conn can't read message")
 	}
+	return typ, body
 }
